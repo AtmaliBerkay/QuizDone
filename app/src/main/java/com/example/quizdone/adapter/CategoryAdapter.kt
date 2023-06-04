12664f46bc723b79fa2a356825d0c8975ca908ba
berkayatmali <eksberk@hotmail.com>
2023-06-04 14:41:07 +0300

    Category Screen(Image from Firebase)

--- app/src/main/java/com/example/quizdone/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/quizdone/adapter/CategoryAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.quizdone.adapter
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.quizdone.databinding.CategoryRowBinding
 import com.example.quizdone.model.CategoryModel
 
@@ -29,13 +32,22 @@ class CategoryAdapter(private val categoryList: ArrayList<CategoryModel>) : Recy
     inner class ViewHolder(private val binding: CategoryRowBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(categoryModel: CategoryModel) {
-           /* val ivCategory: ImageView = binding.ivCategory
+            val ivCategory: ImageView = binding.ivCategory
             Glide.with(itemView.context)
                 .load(categoryModel.quizImgUrl)
-                .into(ivCategory)*/
+                .into(object : CustomTarget<Drawable>() {
+                    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                        // Set the fetched image as the background of the CardView
+                        binding.root.background = resource
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // Optional: You can handle any placeholder or clear background here
+                    }
+                })
             binding.tvQuizDesc.text = categoryModel.quizDesc
             binding.tvQuizTitle.text = categoryModel.quizTitle
-
+            binding.tvQuizId.text = categoryModel.quizId
         }
     }
 }
